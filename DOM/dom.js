d45417081b24d19791361d8558bb566f7190b266
fatima-masmas <87-f@gmx.de>
2023-01-30 18:17:01 +0100

    DOM-Tasks:

--- DOM/dom.js
+++ DOM/dom.js
@@ -6,8 +6,31 @@ hamburger.addEventListener(
     "click", () => {
         hamburger.classList.toggle("active");
         navMenu.classList.toggle("active");
-})
+});
 document.querySelectorAll(".nav-link").forEach(n =>n.addEventListener("click", () => {
     hamburger.classList.remove("active")
     navMenu.classList.remove("active")
-}))
+}));
+
+
+const slider = document.getElementById("slider");
+const boxContainer = document.getElementById("box-container");
+
+slider.addEventListener("input", function() {
+  let n = slider.value;
+  let boxes = "";
+  for (let i = 0; i < n; i++) {
+    let color = getRandomColor();
+    boxes += `<div class="box" style="background-color: ${color}"></div>`;
+  }
+  boxContainer.innerHTML = boxes;
+});
+
+function getRandomColor() {
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
