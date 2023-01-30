7a7c01cd1d54b835f16a165d27669eb36ffee2ad
fatima-masmas <87-f@gmx.de>
2023-01-30 02:36:03 +0100

    file-gelöscht

--- javaScript/calculator.js
+++
@@ -1,8 +0,0 @@
-function add(a, b) {
-    return a + b;
-}
-
-let num1 = parseFloat(prompt("Geben Sie die erste Zahl ein:"));
-let num2 = parseFloat(prompt("Geben Sie die zweite Zahl ein:"));
-let result = add(num1, num2);
-console.log("Das Ergebnis ist: " + result);
